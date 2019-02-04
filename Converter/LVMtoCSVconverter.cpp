5c5568a9c6f109286549deb3f81c9e87baaae4aa
Leonid Pozdneev <leo.pozdneev@gmail.com>
2019-02-04 17:32:16 -0500

    updating code to work with two inputs and adding an example .lvm

--- Converter/LVMtoCSVconverter.cpp
+++ Converter/LVMtoCSVconverter.cpp
@@ -34,6 +34,28 @@
 
 using namespace std;
 
+void convert_time(string *s, bool *start_set, double *start_time) {
+    // find the double space and record everything after it (throw away the date stamp)
+    int space = s->find_first_of("  ");
+    int colon1 = s->find_first_of(":");
+    int colon2 = s->find_last_of(":");
+
+    // find the two colons used in the time stamp, and the tab after the time stamp
+    double hour = stod (s->substr(0, colon1), nullptr);
+    double mins = stod (s->substr(colon1 + 1, colon2), nullptr);
+    double secs = stod (s->substr(colon2 + 1, s->length()), nullptr);
+    double total_secs = hour*3600 + mins*60 + secs;
+
+    // check if the start time as already been recorded
+    if (!(*start_set)) {
+        *start_time = total_secs;
+        *start_set = true;
+    }
+
+    // replce the time stamp with the seconds value, and replace the tab with a comma
+    *s = to_string(total_secs - *start_time);
+}
+
 int main( int argc, char* argv[] )
 {
 
@@ -57,45 +79,48 @@ int main( int argc, char* argv[] )
             infile.open(lvm_file);
             outfile.open(csv_file);
 
-            string initial;
-            string converted;
+            string s;
 
             bool start_set = false;
             double start_time = 0;
             int i = 0;
 
-            while (getline(infile, initial)) {
-                if (i == 0) { // skip the first line
+            while (getline(infile, s)) {
+                if (i == 0) { // replace tabs in the first line with commas
+                    while (s.find("\t") != -1) {
+                        s.replace(s.find("\t"), 1, ",");
+                    }
                     i++;
-                } else {
-                    // find the double space and record everything after it (throw away the date stamp)
-                    int space = initial.find_first_of("  ");
-                    converted = initial.substr(space + 1, initial.length());
+                    outfile << s << "\n";
+                } else { // if it is not the first line, convert the data
 
-                    // find the two colons used in the time stamp, and the tab after the time stamp
-                    int colon1 = converted.find_first_of(":");
-                    int colon2 = converted.find_last_of(":");
-                    int tab = converted.find_first_of("\t");
+                    int tab = s.find_first_of("\t");                 // find the first tab
+                    int space = s.find_first_of("  ");               // find the first double space
+                    string time = s.substr(space + 1, tab);          // information between the tab and the space is the time stamp
+                    convert_time(&time, &start_set, &start_time);    // convert the time stamp into seconds since the beginning
+                    s.replace(0, (s.substr(0, tab).length()), time); // replace the date stamp and the time stamp with the time in seconds
+                    tab = s.find_first_of("\t");                     // find the next tab
+                    s.replace(tab, 1, ",");                          // replace the tab with a comma
 
-                    // convert the time stamp into seconds
-                    double hour = stod (converted.substr(0, colon1), nullptr);
-                    double mins = stod (converted.substr(colon1 + 1, colon2), nullptr);
-                    double secs = stod (converted.substr(colon2 + 1, tab), nullptr);
-                    double total_secs = hour*3600 + mins*60 + secs;
+                    // check if there are more tabs in this line, and convert the data if there is
+                    if (s.find_first_of("\t") != -1) {
 
-                    // check if the start time as already been recorded
-                    if (!start_set) {
-                        start_time = total_secs;
-                        start_set = true;
+                        tab = s.find_first_of("\t");
+                        s.replace(tab, 1, ",");
+                        int comma = s.find_last_of(",");
+                        tab = s.find_first_of("\t");
+                        space = s.find_first_of("  ");
+                        time = s.substr(space + 1, tab);
+                        convert_time(&time, &start_set, &start_time);
+                        s.replace(comma + 1, space - comma + 1, "");
+                        tab = s.find_first_of("\t");
+                        s.replace(tab, 1, ",");
+                        tab = s.find_first_of("\t");
+                        int last_comma = s.find_last_of(",");
+                        s.replace(comma + 1, last_comma - comma - 1, time);
                     }
 
-                    // replce the time stamp with the seconds value, and replace the tab with a comma
-                    string time = to_string(total_secs - start_time);
-                    converted.replace(0, (converted.substr(0, tab).length()), time);
-                    tab = converted.find_first_of("\t");
-                    converted.replace(tab, 1, ",");
-
-                    outfile << converted << "\n";
+                    outfile << s << "\n";
                 }
             }
             infile.close();
@@ -106,3 +131,4 @@ int main( int argc, char* argv[] )
     }
     return 0;
 }
+
